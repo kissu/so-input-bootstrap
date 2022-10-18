98b05abe6a35ac0efdb137d28f9059d80fc02baa
Konstantin BIFERT <hello@kissu.io>
2022-10-18 13:50:09 +0200

    done

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        showPassword: false
+     }
+  }
+}
+</script>
+
+<template>
+  <div>
+    Nice input <b-form-input :type="showPassword ? 'text' : 'password'"></b-form-input>
+
+    <button @click="showPassword = !showPassword">
+      toggle it
+    </button>
+  </div>
+</template>
